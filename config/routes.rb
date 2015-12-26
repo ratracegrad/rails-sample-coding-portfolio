b7e0e14841ced979d039cf8aff85d7444daa67e1
Jennifer Bland <ratracegrad@gmail.com>
2015-12-26 16:22:15 -0500

    Added an about controller and route to handle calls to About me. Create index page to display information about me and added css styling for this page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'pages#welcome'
   get '/portfolio' => 'portfolio#index'
+  get '/about' => 'about#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
